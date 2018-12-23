f6ed284ef8701f77ef5971af9c7cde88eb2c64dc
mrMagic-web <maciejdrahusz@gmail.com>
2018-12-23 14:37:17 +0100

    Zmiana w sekcji sushi i poprawki do jsow

--- js/new-age.js
+++ js/new-age.js
@@ -58,11 +58,17 @@
         dots: false,
         infinite: true,
         speed: 300,
-        slidesToShow: 4,
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2500,
         responsive: [{
+                breakpoint: 1260,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            }, {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
