6be778b8a455a0e4752ca1eac46740849951caf8
user12@cloud-technologies.net <user12@cloud-technologies.net>
2019-04-07 22:15:52 +0200

    Slider and product sections

--- js/new-age.js
+++ js/new-age.js
@@ -1,95 +1,88 @@
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        if (
-            location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate({
-                        scrollTop: target.offset().top - 48
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: target.offset().top - 48
+        },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  $('[data-toggle="tooltip"]').tooltip();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  $('.product-overlay').find('button').on('click', function () {
+    $('.product').removeClass('expanded')
+    $(this).parent().parent().addClass('expanded');
+  })
+  $('.product .description').find('.close-button').on('click', function () {
+    $(this).parent().parent().removeClass('expanded');
+  })
+
+  $('.responsive').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    centerMode: true,
+    centerPadding: '10vw',
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: '0',
         }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function() {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 54
-    });
-
-    // Collapse Navbar
-    var navbarCollapse = function() {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: '0',
         }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    $('[data-toggle="tooltip"]').tooltip();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-
-    $('.product-overlay').find('button').on('click', function() {
-        $('.product').removeClass('expanded')
-        $(this).parent().parent().addClass('expanded');
-    })
-    $('.product .description').find('.close-button').on('click', function() {
-        $(this).parent().parent().removeClass('expanded');
-    })
-
-    $('.responsive').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        responsive: [{
-                breakpoint: 1260,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                }
-            }, {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    });
+      }
+    ]
+  });
 
 })(jQuery); // End of use strict
